914a85705137040a28f1bf8fcb847c5e0a639a68
junyoung-choe <wjdghkswnsdu@naver.com>
2024-08-09 15:49:40 +0900

    build: for redis

---
+++ src/main/java/com/example/threadsafetest/redis/RedissonConfig.java
@@ -0,0 +1,22 @@
+package com.example.threadsafetest.redis;
+
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RedissonConfig {
+
+    private static final String REDISSON_HOST_PREFIX = "redis://";
+
+    @Bean
+    public RedissonClient redissonClient() {
+        RedissonClient redisson = null;
+        Config config = new Config();
+        config.useSingleServer().setAddress(REDISSON_HOST_PREFIX + "localhost:6379");
+        redisson = Redisson.create(config);
+        return redisson;
+    }
+}
